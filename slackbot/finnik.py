d9ff441c1bf23e100032f6e573e6d6dcfbcb64a0
Alexander Bij <AlexanderBij@godatadriven.com>
2019-06-01 01:08:53 +0200

    More strict timeout on Finnik api. Disable service if not in time.

--- slackbot/finnik.py
+++ slackbot/finnik.py
@@ -27,35 +27,44 @@ class FinnikOnlineClient:
             log.warning("Finnik last request failed less than %s sec ago (skip)", self.service_failure_timeout)
             return None
 
-        res = requests.get('https://autorapport.finnik.nl/kenteken/' + plate, timeout=(5, 5))
+        try:
+            # Slack-commands are synchronous, max timeout 3sec
+            # Slack events are asynchronous, than we have enough time.
+            res = requests.get('https://autorapport.finnik.nl/kenteken/' + plate, timeout=(3, 3))
+            res.raise_for_status()
+        except Exception as e:
+            self.enable_service_timeout()
+            log.warning("Request to Finnik failed, Disable service for %s sec.", self.service_failure_timeout)
+            raise e
 
-        if not res.ok:
-            if self.last_failure is not None:
-                next_exp_backoff = int(self.service_failure_timeout * SERVICE_FAILURE_EXPONENTIAL_MULTIPLIER)
-                self.service_failure_timeout = min(SERVICE_FAILURE_MAX_TIMEOUT_SEC, next_exp_backoff)
-            log.warning("Unsuccessful lookup: (%s) %s. Disable service for %s sec.", res.status_code, res.reason, self.service_failure_timeout)
-            self.last_failure = dt.datetime.now()
-            return None
-
-        # reset after success:
-        self.last_failure = None
-        self.service_failure_timeout = DEFAULT_SERVICE_FAILURE_TIMEOUT_SEC
+        self.disable_service_timeout()
 
         soup = BeautifulSoup(res.content, "html.parser")
 
         div = soup.find("div", id="summary-new")
         if not div:
-            log.warning("Successful response, but element ('div', id='summary-new') not found! Is the site changed? Disable service for %s sec.",
-                        self.service_failure_timeout)
+            log.warning("Successful response, but element ('div', id='summary-new') not found! "
+                        "Is the site changed? Disable service for %s sec.", self.service_failure_timeout)
             self.last_failure = dt.datetime.now()
             return None
 
         acceleration_item = div.find(id="value-acceleratie")
         if not acceleration_item:
-            log.warning("Successful response, but element (id='value-acceleratie') not found! Is the site changed? Disable service for %s sec.",
-                        self.service_failure_timeout)
+            log.warning("Successful response, but element (id='value-acceleratie') not found! "
+                        "Is the site changed? Disable service for %s sec.", self.service_failure_timeout)
             self.last_failure = dt.datetime.now()
             return None
 
         acceleration = acceleration_item.text.replace("seconden", "").strip()
+        log.info("Acceleration lookup for %s from Finnik result: {} sec", plate, acceleration)
         return acceleration
+
+    def enable_service_timeout(self):
+        if self.last_failure is not None:
+            next_exp_backoff = int(self.service_failure_timeout * SERVICE_FAILURE_EXPONENTIAL_MULTIPLIER)
+            self.service_failure_timeout = min(SERVICE_FAILURE_MAX_TIMEOUT_SEC, next_exp_backoff)
+        self.last_failure = dt.datetime.now()
+
+    def disable_service_timeout(self):
+        self.last_failure = None
+        self.service_failure_timeout = DEFAULT_SERVICE_FAILURE_TIMEOUT_SEC
