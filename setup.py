3464b9c5c2bffc7452788d9138ec57eb44a1b992
Alexander Bij <AlexanderBij@godatadriven.com>
2022-06-17 15:23:31 +0200

    Dump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='slackbot',
-    version='1.0',
+    version='1.1',
     description='Car Lookup - Slackbot',
     author='Alexander Bij',
     author_email='AlexanderBij@GoDataDriven.com',
