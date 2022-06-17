9d9c357f81e90ba7ab7cfb8755f02ee0be13f911
Alexander Bij <AlexanderBij@godatadriven.com>
2022-06-17 14:34:11 +0200

    Ignore INFO logging

--- slackbot/__init__.py
+++ slackbot/__init__.py
@@ -3,3 +3,6 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)-4.4s] [%(name)s:%(lineno)d]  %(message)s",
     handlers=[logging.StreamHandler()])
+
+# Skip all the spamming INFO
+logging.getLogger('werkzeug').setLevel(logging.WARNING)
